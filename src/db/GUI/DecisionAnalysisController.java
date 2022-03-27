914c61f90a85dc88adf1ebdadad215787cce4591
pmsierra <p.martin61@usp.ceu.es>
2022-03-27 21:10:41 +0200

    Rules work

--- src/db/GUI/DecisionAnalysisController.java
+++ src/db/GUI/DecisionAnalysisController.java
@@ -2,8 +2,16 @@ package db.GUI;
 
 import java.io.IOException;
 import java.net.URL;
+import java.sql.SQLException;
+import java.util.Collections;
+import java.util.Comparator;
+import java.util.LinkedList;
 import java.util.List;
 import java.util.ResourceBundle;
+
+import org.kie.api.KieServices;
+import org.kie.api.runtime.KieContainer;
+import org.kie.api.runtime.KieSession;
 
 import com.jfoenix.controls.JFXButton;
 import com.jfoenix.controls.JFXTreeTableColumn;
@@ -12,6 +20,7 @@ import com.jfoenix.controls.RecursiveTreeItem;
 import com.jfoenix.controls.datamodels.treetable.RecursiveTreeObject;
 
 import droolsexample.priority.SQLiteManager;
+import droolsexample.priority.SQLiteMethods;
 import javafx.beans.property.SimpleStringProperty;
 import javafx.beans.property.StringProperty;
 import javafx.beans.value.ObservableValue;
@@ -34,7 +43,9 @@ import java.util.ResourceBundle;
 
 import com.jfoenix.controls.JFXButton;
 
+import droolsexample.priority.Department;
 import droolsexample.priority.Hospital;
+import droolsexample.priority.Resource;
 import droolsexample.priority.SQLiteManager;
 import javafx.event.Event;
 import javafx.event.EventHandler;
@@ -70,9 +81,9 @@ public class DecisionAnalysisController implements Initializable{
 	@FXML
 	private JFXButton compute_decision_button;
 	@FXML
-	private JFXTreeTableView<BougthItems> bought_tree_view;
+	private JFXTreeTableView<BoughtItems> bought_tree_view;
 	@FXML
-	private final ObservableList<BougthItems> bought_objects = FXCollections.observableArrayList();
+	private final ObservableList<BoughtItems> bought_objects = FXCollections.observableArrayList();
 	
 	public DecisionAnalysisController() {
 		// TODO Auto-generated constructor stub
@@ -85,102 +96,145 @@ public class DecisionAnalysisController implements Initializable{
 	
 	@FXML
 	private void decision_button(MouseEvent event) throws IOException {
-		JFXTreeTableColumn<BougthItems, String> worker_name = new JFXTreeTableColumn<>("Worker name");
-		worker_name.setPrefWidth(180);
-		worker_name.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<BougthItems,String>, ObservableValue<String>>() {
-			@Override
-			public ObservableValue<String> call(CellDataFeatures<BougthItems, String> param) {
-				return param.getValue().getValue().worker_name;
-			}
-		});
-		worker_name.setResizable(false);
-		
-		JFXTreeTableColumn<BougthItems, String> user_name = new JFXTreeTableColumn<>("User name");
-		user_name.setPrefWidth(180);
-		user_name.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<BougthItems,String>, ObservableValue<String>>() {
-			@Override
-			public ObservableValue<String> call(CellDataFeatures<BougthItems, String> param) {
-				return param.getValue().getValue().user_name;
-			}
-		});
-		user_name.setResizable(false);
-		
-		JFXTreeTableColumn<BougthItems, String> worker_telephone = new JFXTreeTableColumn<>("Telephone");
-		worker_telephone.setPrefWidth(180);
-		worker_telephone.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<BougthItems,String>, ObservableValue<String>>() {
-			@Override
-			public ObservableValue<String> call(CellDataFeatures<BougthItems, String> param) {
-				return param.getValue().getValue().worker_telephone;
-			}
-		});
-		worker_telephone.setResizable(false);
-		
-		JFXTreeTableColumn<BougthItems, String> worker_email = new JFXTreeTableColumn<>("Email");
-		worker_email.setPrefWidth(180);
-		worker_email.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<BougthItems,String>, ObservableValue<String>>() {
-			@Override
-			public ObservableValue<String> call(CellDataFeatures<BougthItems, String> param) {
-				return param.getValue().getValue().worker_email;
-			}
-		});
-		worker_email.setResizable(false);
-		
-		JFXTreeTableColumn<BougthItems, String> worker_password = new JFXTreeTableColumn<>("Password");
-		worker_password.setPrefWidth(180);
-		worker_password.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<BougthItems,String>, ObservableValue<String>>() {
-			@Override
-			public ObservableValue<String> call(CellDataFeatures<BougthItems, String> param) {
-				return param.getValue().getValue().user_password;
-			}
-		});
-		worker_password.setResizable(false);
-			
-		List<Worker> workers_list = manager_object.List_all_workers();
-		for (Worker worker: workers_list) {
-			bougth_objects.add(new BougthItems(worker.getEmail(), worker.getUser().getUserName(), worker.getWorker_name(), worker.getTelephone().toString(), worker.getUser().getPassword()));
-		}
-		TreeItem<BougthItems> root = new RecursiveTreeItem<BougthItems>(bougth_objects, RecursiveTreeObject::getChildren);
-		bougth_tree_view.setPlaceholder(new Label("No workers found"));
-		bougth_tree_view.getColumns().setAll(user_name, worker_name, worker_email, worker_telephone, worker_password);
-		bougth_tree_view.setRoot(root);
-		bougth_tree_view.setShowRoot(false);
+		   KieServices ks = KieServices.Factory.get();
+	       KieContainer kc = ks.getKieClasspathContainer();
+	       System.out.println("kk");
+
+	        execute(ks, kc);
+	        refreshItemsListView();
 	}
+    public static void execute(KieServices ks, KieContainer kc) {
 	
+		KieSession ksession = kc.newKieSession("PriorityKS");
+	    System.out.println("kk2");
+		
+		
+        // Once the session is created, the application can interact with it
+    
+	    Hospital hospital = manager_object.getMethods().List_all_hospitals().get(0);
+	    System.out.println(hospital.toString());
+	    hospital.calculatePriorityList();
+	    System.out.println(hospital.toString());
+        ksession.insert(hospital);
+        ksession.insert(manager_object);
+        
+        
+        // and fire the rules
+        ksession.fireAllRules();
+
+
+        // and then dispose the session
+        ksession.dispose();
+
+    }
+
+    private static void sort(LinkedList<Resource> list) {
+    	Collections.sort(list, new Comparator<Resource>() {
+    	@Override
+    		public int compare(Resource o1, Resource o2) {
+                if (o1.getPriority() == o2.getPriority()) {
+                    return o1.getName().compareTo(o2.getName());
+                } else {
+                    return o1.getPriority().compareTo(o2.getPriority());
+                }
+            }
+    		});
+    }
+
 	@Override @SuppressWarnings("unchecked")
 	public void initialize(URL location, ResourceBundle resources) {
-		
+;
 		// Worker list columns creation
+		JFXTreeTableColumn<BoughtItems, String> department_name = new JFXTreeTableColumn<>("Department name");
+		department_name.setPrefWidth(100);
+		department_name.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<BoughtItems,String>, ObservableValue<String>>() {
+			@Override
+			public ObservableValue<String> call(CellDataFeatures<BoughtItems, String> param) {
+				return param.getValue().getValue().department_name;
+			}
+		});
+		department_name.setResizable(false);
+		
+		JFXTreeTableColumn<BoughtItems, String> item_name = new JFXTreeTableColumn<>("Item Name");
+		item_name.setPrefWidth(100);
+		item_name.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<BoughtItems,String>, ObservableValue<String>>() {
+			@Override
+			public ObservableValue<String> call(CellDataFeatures<BoughtItems, String> param) {
+				return param.getValue().getValue().item_name;
+			}
+		});
+		item_name.setResizable(false);
+		
+		JFXTreeTableColumn<BoughtItems, String> price = new JFXTreeTableColumn<>("Price");
+		price.setPrefWidth(100);
+		price.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<BoughtItems,String>, ObservableValue<String>>() {
+			@Override
+			public ObservableValue<String> call(CellDataFeatures<BoughtItems, String> param) {
+				return param.getValue().getValue().price;
+			}
+		});
+		price.setResizable(false);
+		
+		JFXTreeTableColumn<BoughtItems, String> priority_item = new JFXTreeTableColumn<>("Priority of Item");
+		priority_item.setPrefWidth(100);
+		priority_item.setCellValueFactory(new Callback<TreeTableColumn.CellDataFeatures<BoughtItems,String>, ObservableValue<String>>() {
+			@Override
+			public ObservableValue<String> call(CellDataFeatures<BoughtItems, String> param) {
+				return param.getValue().getValue().priority_item;
+			}
+		});
+		priority_item.setResizable(false);
+		
+		
+		
+		LinkedList<Resource> boughtItems = hospital_account.getBougthItems();
+		LinkedList<Department> departmentOrder = hospital_account.getDepartmentOrder();
+		for (Department department: departmentOrder) {
+			int position = departmentOrder.indexOf(department);
+			Resource thisItem = boughtItems.get(position);
+			bought_objects.add(new BoughtItems(department.getName(), thisItem.getName(), thisItem.getPrice().toString(), thisItem.getPriority()));
+		}
+		TreeItem<BoughtItems> root = new RecursiveTreeItem<BoughtItems>(bought_objects, RecursiveTreeObject::getChildren);
+		bought_tree_view.setPlaceholder(new Label("No Departments found"));
+		bought_tree_view.getColumns().setAll(department_name, item_name, price, priority_item);
+		bought_tree_view.setRoot(root);
+		bought_tree_view.setShowRoot(false);
+	
+	
+
 		
 	}
 
-// -----> REFRESH WORKERS LIST VIEW <-----
+// -----> REFRESH ITEMS LIST VIEW <-----
 
-	public void refreshWorkerListView() {
-		bougth_objects.clear();
-		List<Worker> workers_list = manager_object.List_all_workers();
-		for (Worker worker: workers_list) {
-			bougth_objects.add(new BougthItems(worker.getEmail(), worker.getUser().getUserName(), worker.getWorker_name(), worker.getTelephone().toString(), worker.getUser().getPassword()));
+	public void refreshItemsListView() {
+	    System.out.println("kk5");
+		LinkedList<Resource> boughtItems = hospital_account.getBougthItems();
+		LinkedList<Department> departmentOrder = hospital_account.getDepartmentOrder();
+		for (Department department: departmentOrder) {
+			int position = departmentOrder.indexOf(department);
+			Resource thisItem = boughtItems.get(position);
+		    System.out.println("this Item: " + thisItem);
+			bought_objects.add(new BoughtItems(department.getName(), thisItem.getName(), thisItem.getPrice().toString(), thisItem.getPriority()));
 		}
-		TreeItem<BougthItems> root = new RecursiveTreeItem<BougthItems>(bougth_objects, RecursiveTreeObject::getChildren);
-		bougth_tree_view.refresh();
+		TreeItem<BoughtItems> root = new RecursiveTreeItem<BoughtItems>(bought_objects, RecursiveTreeObject::getChildren);
+		bought_tree_view.refresh();
 	}
 }
 
 //To insert columns into the list of workers with all the information
-class BougthItems extends RecursiveTreeObject<BougthItems> {
+class BoughtItems extends RecursiveTreeObject<BoughtItems> {
 	
-	StringProperty user_name;
-	StringProperty worker_name;
-	StringProperty worker_telephone;
-	StringProperty worker_email;
-	StringProperty user_password;
+	StringProperty department_name;
+	StringProperty item_name;
+	StringProperty price;
+	StringProperty priority_item;
 	
-	public BougthItems(String worker_email, String user_name, String worker_name, String worker_telephone, String password) {
-		this.worker_name = new SimpleStringProperty(worker_name);
-		this.user_name = new SimpleStringProperty(user_name);
-		this.worker_telephone = new SimpleStringProperty(worker_telephone);
-		this.worker_email = new SimpleStringProperty(worker_email);
-		this.user_password = new SimpleStringProperty(password);
+	public BoughtItems(String department_name, String item_name, String price, String priority_item) {
+		this.department_name = new SimpleStringProperty(department_name);
+		this.item_name = new SimpleStringProperty(item_name);
+		this.price = new SimpleStringProperty(price);
+		this.priority_item = new SimpleStringProperty(priority_item);
 	}
 }
 
