d2959c09abad2cba707737e30f4978d331c8bc51
pmsierra <p.martin61@usp.ceu.es>
2022-03-27 15:58:35 +0200

    changing some views

--- src/db/GUI/DecisionAnalysisController.java
+++ src/db/GUI/DecisionAnalysisController.java
@@ -27,6 +27,34 @@ import javafx.scene.control.TreeTableColumn.CellDataFeatures;
 import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.Pane;
 import javafx.util.Callback;
+import java.io.IOException;
+import java.net.URL;
+
+import java.util.ResourceBundle;
+
+import com.jfoenix.controls.JFXButton;
+
+import droolsexample.priority.Hospital;
+import droolsexample.priority.SQLiteManager;
+import javafx.event.Event;
+import javafx.event.EventHandler;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+
+import javafx.scene.control.Label;
+
+import javafx.scene.effect.BoxBlur;
+import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
+import javafx.scene.layout.AnchorPane;
+import javafx.scene.layout.Pane;
+import javafx.stage.Modality;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
+import javafx.stage.WindowEvent;
 
 public class DecisionAnalysisController implements Initializable{
 
